975a5141125f50482ea0185330b490419a78671f
Kilian Holzinger <kilian.holzinger@gmail.com>
2020-08-03 22:20:31 +0200

    Add newsxml and firefox

--- update-all.sh
+++ update-all.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-for PROJECT in i2p.itoopie i2p.plugins.zzzot i2p.graphics i2p.plugins.i2pcontrol i2p.scripts i2p.android.base i2p.syndie i2p.i2p i2p.www i2p.plugins.orchid; do
+for PROJECT in i2p.itoopie i2p.plugins.zzzot i2p.graphics i2p.plugins.i2pcontrol i2p.scripts i2p.android.base i2p.syndie i2p.i2p i2p.www i2p.plugins.orchid i2p.newsxml i2p.firefox; do
     ./update-git.sh "$PROJECT" || echo "Export of $PROJECT failed"
 done
